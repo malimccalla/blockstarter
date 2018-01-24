dbe67348e4092f82003053b5a2f871677ea35e06
Mali Michael <malimccalla@gmail.com>
2018-01-24 22:59:00 +0000

    Deploy script

---
+++ ethereum/constants.js
@@ -0,0 +1,1 @@
+export const factoryAddress = '0xEFC59618d44427095D8E3Ee37c1930D76889Ac65';
---
+++ ethereum/deploy.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const comipledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+  process.env.DEV_MNEMOIC,
+  process.env.INFURA_INSTANCE
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(comipledFactory.interface)
+  )
+    .deploy({ data: comipledFactory.bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
