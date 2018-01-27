8b0667cb529ca1537dc5a3e1d164580483a25b4d
Mali Michael <malimccalla@gmail.com>
2018-01-27 09:29:42 +0000

    Note fix

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -3,7 +3,7 @@ import Web3 from 'web3';
 let web3;
 
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
-  // in the browser and metamask is running metamask
+  // in the browser and user is running metamask
   web3 = new Web3(window.web3.currentProvider);
 } else {
   // on the server OR user is not running metamask
